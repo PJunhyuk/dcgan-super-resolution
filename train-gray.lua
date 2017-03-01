761dfe84f4bd890c35c6ddd0b1194a15c447f15d
PJunhyuk <jgravity@naver.com>
2017-03-01 13:06:15 +0900

    Change variable of convolution in netG

--- train-gray.lua
+++ train-gray.lua
@@ -161,20 +161,16 @@ local netG = nn.Sequential()
 netG:add(nn.SpatialUpSamplingNearest(2))
 netG:add(SpatialBatchNormalization(nc))
 -- nc x 64 x 64
--- netG:add(SpatialFullConvolution(nc, ngf*4, 4, 4, 2, 2, 1, 1))
-netG:add(SpatialFullConvolution(nc, ngf*4, 2, 2, 2, 2))
+netG:add(SpatialFullConvolution(nc, ngf*4, 4, 4, 2, 2, 0, 0))
 netG:add(SpatialBatchNormalization(ngf*4))
--- ngf*4 x 128 x 128
--- netG:add(SpatialFullConvolution(ngf*4, ngf*2, 4, 4, 2, 2, 1, 1))
-netG:add(SpatialFullConvolution(ngf*4, ngf*2, 2, 2, 2, 2))
+-- ngf*4 x 130 x 130
+netG:add(SpatialFullConvolution(ngf*4, ngf*2, 4, 4, 2, 2, 0, 0))
 netG:add(SpatialBatchNormalization(ngf*2))
--- ngf*2 x 256 x 256
--- netG:add(SpatialConvolution(ngf*2, ngf, 4, 4, 2, 2, 1, 1))
-netG:add(SpatialConvolution(ngf*2, ngf, 2, 2, 2, 2))
+-- ngf*2 x 264 x 264
+netG:add(SpatialConvolution(ngf*2, ngf, 4, 4, 2, 2, 0, 0))
 netG:add(SpatialBatchNormalization(ngf))
--- ngf x 128 x 128
--- netG:add(SpatialConvolution(ngf, nc, 4, 4, 2, 2, 1, 1))
-netG:add(SpatialConvolution(ngf, nc, 2, 2, 2, 2))
+-- ngf x 130 x 130
+netG:add(SpatialConvolution(ngf, nc, 4, 4, 2, 2, 0, 0))
 netG:add(SpatialBatchNormalization(nc))
 -- nc x 64 x 64
 
