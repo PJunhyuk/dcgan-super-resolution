ead0cf4eb0107a6115329fddffdc7e383490eb6f
PJunhyuk <jgravity@naver.com>
2017-03-16 00:21:10 +0900

    Change structure of netG - back to default

--- train-gray.lua
+++ train-gray.lua
@@ -159,27 +159,23 @@ local SpatialAveragePooling = nn.SpatialAveragePooling
 local netG = nn.Sequential()
 -- nc x 32 x 32
 
-netG:add(nn.SpatialUpSamplingNearest(2))
-netG:add(SpatialBatchNormalization(nc))
--- nc x 64 x 64
-
--- netG:add(SpatialFullConvolution(nc, ngf, 2, 2, 2, 2))
--- netG:add(SpatialBatchNormalization(ngf))
+netG:add(SpatialFullConvolution(nc, ngf, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialBatchNormalization(ngf))
 -- ngf x 64 x 64
 
-netG:add(SpatialFullConvolution(nc, ngf*2, 4, 4, 2, 2, 0, 0))
+netG:add(SpatialFullConvolution(ngf, ngf*2, 4, 4, 2, 2, 1, 1))
 netG:add(SpatialBatchNormalization(ngf*2))
--- ngf*2 x 130 x 130
+-- ngf*2 x 128 x 128
 
-netG:add(SpatialFullConvolution(ngf*2, ngf*4, 4, 4, 2, 2, 0, 0))
+netG:add(SpatialFullConvolution(ngf*2, ngf*4, 4, 4, 2, 2, 1, 1))
 netG:add(SpatialBatchNormalization(ngf*4))
--- ngf*4 x 262 x 262
+-- ngf*4 x 256 x 256
 
-netG:add(SpatialConvolution(ngf*4, ngf*8, 4, 4, 2, 2, 0, 0))
-netG:add(SpatialBatchNormalization(ngf*8))
--- ngf*8 x 130 x 130
+netG:add(SpatialConvolution(ngf*4, ngf, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialBatchNormalization(ngf))
+-- ngf x 128 x 128
 
-netG:add(SpatialConvolution(ngf*8, nc, 4, 4, 2, 2, 0, 0))
+netG:add(SpatialConvolution(ngf, nc, 4, 4, 2, 2, 1, 1))
 netG:add(SpatialBatchNormalization(nc))
 -- nc x 64 x 64
 
