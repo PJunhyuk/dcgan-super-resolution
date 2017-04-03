0c561b2779b9f7f4fe66bcfadecf01487f4d1cd5
PJunhyuk <jgravity@naver.com>
2017-04-03 23:06:32 +0900

    Use image.scale function

--- train-gray.lua
+++ train-gray.lua
@@ -346,19 +346,23 @@ function testSample(real_none_color_sample)
     print(('real_reduced_sample-sum: %.8f  real_reduced_sample-std: %.8f'):format(real_reduced_sample:sum(), real_reduced_sample:std()))
 
     local real_none_2x_sample = torch.Tensor(opt.fineSize, opt.fineSize)
-    for i = 1, opt.fineSize/2 do
-        for j = 1, opt.fineSize/2 do
-            real_none_2x_sample[{ {2*i}, {2*j} }] = real_reduced_sample[{ {i}, {j} }]
-            real_none_2x_sample[{ {2*i-1}, {2*j} }] = real_reduced_sample[{ {i}, {j} }]
-            real_none_2x_sample[{ {2*i}, {2*j-1} }] = real_reduced_sample[{ {i}, {j} }]
-            real_none_2x_sample[{ {2*i-1}, {2*j-1} }] = real_reduced_sample[{ {i}, {j} }]
-        end
-    end
-    -- real_none_2x_sample = normalizeImg2(real_none_2x_sample)
+    -- for i = 1, opt.fineSize/2 do
+    --     for j = 1, opt.fineSize/2 do
+    --         real_none_2x_sample[{ {2*i}, {2*j} }] = real_reduced_sample[{ {i}, {j} }]
+    --         real_none_2x_sample[{ {2*i-1}, {2*j} }] = real_reduced_sample[{ {i}, {j} }]
+    --         real_none_2x_sample[{ {2*i}, {2*j-1} }] = real_reduced_sample[{ {i}, {j} }]
+    --         real_none_2x_sample[{ {2*i-1}, {2*j-1} }] = real_reduced_sample[{ {i}, {j} }]
+    --     end
+    -- end
+    -- -- real_none_2x_sample = normalizeImg2(real_none_2x_sample)
+
+    real_none_2x_sample = image.scale(real_reduced_sample, opt.fineSize, opt.fineSize, bilinear)
+
     image.save('real_none_2x_sample.png', image.toDisplayTensor(real_none_2x_sample))
 
     print(('real_none_2x_sample-max: %.8f  real_none_2x_sample-min: %.8f'):format(real_none_2x_sample:max(), real_none_2x_sample:min()))
     print(('real_none_2x_sample-sum: %.8f  real_none_2x_sample-std: %.8f'):format(real_none_2x_sample:sum(), real_none_2x_sample:std()))
+
 
     local inputG_sample = torch.Tensor(1, 1, opt.fineSize/2, opt.fineSize/2)
     inputG_sample[{{1}, {1}, {}, {}}] = real_reduced_sample[{ {}, {}}]
