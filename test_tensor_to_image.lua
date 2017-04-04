cc2b17fa6adeb5bd779717a754a72ec7ac29291e
PJunhyuk <jgravity@naver.com>
2017-04-05 02:38:41 +0900

    Change tensor to float in test_tensor_to_image

--- test_tensor_to_image.lua
+++ test_tensor_to_image.lua
@@ -3,6 +3,7 @@ require 'image'
 
 local image_all_0 = torch.Tensor(64, 64)
 image_all_0:fill(0)
+image_all_0:float()
 image.save('image_all_0.png', image.toDisplayTensor(image_all_0))
 
 print(('image_all_0-max: %.4f  image_all_0-min: %.4f'):format(image_all_0:max(), image_all_0:min()))
@@ -10,6 +11,7 @@ print(('image_all_0-sum: %.4f  image_all_0-std: %.4f'):format(image_all_0:sum(),
 
 local image_all_05 = torch.Tensor(64, 64)
 image_all_05:fill(0.5)
+image_all_05:float()
 image.save('image_all_05.png', image.toDisplayTensor(image_all_05))
 
 print(('image_all_05-max: %.4f  image_all_05-min: %.4f'):format(image_all_05:max(), image_all_05:min()))
@@ -17,6 +19,7 @@ print(('image_all_05-sum: %.4f  image_all_05-std: %.4f'):format(image_all_05:sum
 
 local image_all_1 = torch.Tensor(64, 64)
 image_all_1:fill(1)
+image_all_1:float()
 image.save('image_all_1.png', image.toDisplayTensor(image_all_1))
 
 print(('image_all_1-max: %.4f  image_all_1-min: %.4f'):format(image_all_1:max(), image_all_1:min()))
@@ -24,6 +27,7 @@ print(('image_all_1-sum: %.4f  image_all_1-std: %.4f'):format(image_all_1:sum(),
 
 local image_all__1 = torch.Tensor(64, 64)
 image_all__1:fill(-1)
+image_all__1:float()
 image.save('image_all__1.png', image.toDisplayTensor(image_all__1))
 
 print(('image_all__1-max: %.4f  image_all__1-min: %.4f'):format(image_all__1:max(), image_all__1:min()))
@@ -31,6 +35,7 @@ print(('image_all__1-sum: %.4f  image_all__1-std: %.4f'):format(image_all__1:sum
 
 local image_all__05 = torch.Tensor(64, 64)
 image_all__05:fill(-0.5)
+image_all__05:float()
 image.save('image_all__05.png', image.toDisplayTensor(image_all__05))
 
 print(('image_all__05-max: %.4f  image_all__05-min: %.4f'):format(image_all__05:max(), image_all__05:min()))
