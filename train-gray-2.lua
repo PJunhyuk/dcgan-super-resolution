fe6131a852fef753f512a0850908ee7850547054
PJunhyuk-mac <jgravity@naver.com>
2017-04-05 15:52:10 +0900

    Add image scaling

--- train-gray-2.lua
+++ train-gray-2.lua
@@ -25,20 +25,19 @@ require 'image'
 -- 	return z
 -- end
 
+local sample_size = 64
+
+
 local file_name_route = '/CelebA/Img/img_align_celeba/Img/'
 
 local file_num = 1
 
 local file_name = file_name_route .. '00000' .. tostring(file_num) .. '.jpg'
 
-print(file_name)
-
-local image_input_color = image.load(file_name, 3, 'float')
-image.save('image_input_color.jpg', image.toDisplayTensor(image_input_color))
-
 local image_input_gray = image.load(file_name, 1, 'float')
-image.save('image_input_gray.jpg', image.toDisplayTensor(image_input_gray))
 
--- rgb2gray(image_input_color)
+image.scale(image_input_gray, sample_size, sample_size)
+
+image.save('image_input_gray.jpg', image.toDisplayTensor(image_input_gray))
 
 print(image_input_gray)
