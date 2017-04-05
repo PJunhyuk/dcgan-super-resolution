3d6984fc82a5a3afab2e8bc179dc9f9bab6c7693
PJunhyuk-mac <jgravity@naver.com>
2017-04-05 15:36:04 +0900

    Fix typo - toString to tostring

--- train-gray-2.lua
+++ train-gray-2.lua
@@ -5,7 +5,7 @@ local file_name_route = '/CelebA/Img/img_align_celeba/Img/'
 
 local file_num = 1
 
-local file_name = file_name_route .. '00000' .. toString(file_num) .. '.jpg'
+local file_name = file_name_route .. '00000' .. tostring(file_num) .. '.jpg'
 
 print(file_name)
 
