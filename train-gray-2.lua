b2baebed0ad46d40e64859431301e10679f33b2e
PJunhyuk-mac <jgravity@naver.com>
2017-04-05 15:33:07 +0900

    Add train-gray-2.lua

---
+++ train-gray-2.lua
@@ -0,0 +1,14 @@
+require 'torch'
+require 'image'
+
+local file_name_route = '/CelebA/Img/img_align_celeba/Img/'
+
+local file_num = 1
+
+local file_name = file_name_route + '00000' + toString(file_num) + '.jpg'
+
+print(file_name)
+
+local image_input = image.load(file_name, 3, 'byte')
+
+print(image_input)
