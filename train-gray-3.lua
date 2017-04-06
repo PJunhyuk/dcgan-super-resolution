07e8e85b845405ce05b4999366f6d018976760d0
PJunhyuk <jgravity@naver.com>
2017-04-07 03:05:16 +0900

    Change netG

--- train-gray-3.lua
+++ train-gray-3.lua
@@ -78,10 +78,10 @@ netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
 netG:add(SpatialFullConvolution(ngf * 4, ngf * 2, 4, 4, 2, 2, 1, 1))
 netG:add(SpatialBatchNormalization(ngf * 2)):add(nn.ReLU(true))
 -- ngf*2 x 256 x 256
-netG:add(SpatialConvolution(ngf * 2, ngf * 4, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialConvolution(ngf * 2, ngf * 4, 2, 2, 2, 2))
 netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
 -- ngf*4 x 128 x 128
-netG:add(SpatialConvolution(ngf * 4, nc, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialConvolution(ngf * 4, nc, 2, 2, 2, 2))
 netG:add(nn.Sigmoid())
 -- nc x 64 x 64
 netG:apply(weights_init)
