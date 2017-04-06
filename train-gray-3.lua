30e87cbde8cf5041297d5bfb5896d7b39dbf7372
PJunhyuk <jgravity@naver.com>
2017-04-07 03:41:01 +0900

    Change structure of netG

--- train-gray-3.lua
+++ train-gray-3.lua
@@ -75,13 +75,13 @@ netG:add(SpatialBatchNormalization(ngf * 8)):add(nn.ReLU(true))
 netG:add(SpatialFullConvolution(ngf * 8, ngf * 4, 2, 2, 2, 2))
 netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
 -- ngf*4 x 128 x 128
-netG:add(SpatialFullConvolution(ngf * 4, ngf * 2, 2, 2, 2, 2))
-netG:add(SpatialBatchNormalization(ngf * 2)):add(nn.ReLU(true))
--- ngf*2 x 256 x 256
-netG:add(SpatialConvolution(ngf * 2, ngf * 4, 4, 4, 2, 2, 1, 1))
-netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
--- ngf*4 x 128 x 128
-netG:add(SpatialConvolution(ngf * 4, nc, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialFullConvolution(ngf * 4, nc, 2, 2, 2, 2))
+netG:add(SpatialBatchNormalization(nc)):add(nn.ReLU(true))
+-- nc x 256 x 256
+netG:add(nn.SpatialAveragePooling(2, 2, 2, 2, 0, 0))
+netG:add(SpatialBatchNormalization(nc)):add(nn.ReLU(true))
+-- nc x 128 x 128
+netG:add(nn.SpatialAveragePooling(2, 2, 2, 2, 0, 0))
 netG:add(nn.Sigmoid())
 -- nc x 64 x 64
 netG:apply(weights_init)
