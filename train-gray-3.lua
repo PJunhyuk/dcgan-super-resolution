372dff34c8cda4df2ea29ebe69f86528298e078b
PJunhyuk <jgravity@naver.com>
2017-04-15 17:34:48 +0900

    Remove useless parts

--- train-gray-3.lua
+++ train-gray-3.lua
@@ -49,25 +49,6 @@ local nc = 1
 local ndf = opt.ndf
 local ngf = opt.ngf
 
--- -- set network of Generator
--- local netG = nn.Sequential()
--- -- nc x 32 x 32
--- netG:add(nn.SpatialUpSamplingNearest(2))
--- -- nc x 64 x 64
--- netG:add(SpatialFullConvolution(nc, ngf * 4, 4, 4, 2, 2, 1, 1))
--- netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
--- -- ngf*4 x 128 x 128
--- netG:add(SpatialFullConvolution(ngf * 4, ngf * 2, 4, 4, 2, 2, 1, 1))
--- netG:add(SpatialBatchNormalization(ngf * 2)):add(nn.ReLU(true))
--- -- ngf*2 x 256 x 256
--- netG:add(SpatialConvolution(ngf * 2, ngf, 4, 4, 2, 2, 1, 1))
--- netG:add(SpatialBatchNormalization(ngf)):add(nn.ReLU(true))
--- -- ngf x 128 x 128
--- netG:add(SpatialConvolution(ngf, nc, 4, 4, 2, 2, 1, 1))
--- netG:add(nn.Sigmoid())
--- -- nc x 64 x 64
--- netG:apply(weights_init)
-
 -- set network of Generator
 local netG = nn.Sequential()
 -- nc x 32
